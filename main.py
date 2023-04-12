5f826df9df7890e63a522b32db13067a6185a5bd
unknown <acharafavour01@gmail.com>
2023-04-12 17:20:44 +0100

    learning python

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Welcome to Veritas University')
+name=input('What is your name')
+password=input('Enter your password before continuing: ')
+if password=='VERITAS'
+    print('Welcome to Veritas University Student Portal')
+else:
+    print('Try again')
+level=input('What is your level: ')
+
